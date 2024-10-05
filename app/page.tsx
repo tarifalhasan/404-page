5f83169d4256c53721f856560b2c69aa584f2b55
tarifalhasan <tarifalhasan999@gmail.com>
2024-10-05 13:57:50 +0600

    updating

--- app/page.tsx
+++ app/page.tsx
@@ -2,100 +2,47 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className=" relative min-h-screen h-full grid place-items-center  ">
+      <main
+        style={{
+          backgroundSize: "100% 100%", // Forces the background to stretch to 100% width and height
+          backgroundPosition: "center", // Centers the background image
+        }}
+        className="min-h-[calc(100vh-50px)]  flex flex-col lg:flex-row gap-4 w-full  mx-  bg-[url('/bg.png')]  px-4 py-6 overflow-hidden xl:py-10 bg-no-repeat  max-w-[calc(100%-40px)] relative lg:items-center"
+      >
+        <div className="absolute  bottom-[96%]">
+          <h3 className="text-[23px] font-minecraft font-normal uppercase">
+            Passs<span className="text-[#FFD600]">Phrase</span>
+          </h3>
+        </div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+        <div className="basis-full lg:basis-[55%]">
+          <Image
+            src={"/8a9ae5c99ad0a75c9646a77e4d421e8c.gif"}
+            alt="sdsd"
+            width={817}
+            height={617}
+            className="max-h-[600px] object-contain"
+          />
+        </div>
+        <div className="basis-full space-y-6 lg:basis-[45%]">
+          <div>
+            <h2 className="text-[187px] leading-none font-minecraft">404</h2>
+          </div>
+          <ul className="space-y-3">
+            <li className="text-base uppercase font-light">Not_found</li>
+            <li className="text-base uppercase font-light">
+              Code: ‘Not_found’
+            </li>
+            <li className="text-base uppercase font-light">
+              ID: ‘sinl : : txbkm-1649130014806-6514bbf1cd63’
+            </li>
+          </ul>
+          <button className="bg-yellow-400 px-6 py-3 text-black">
+            Click here to learn more about this error
+          </button>
         </div>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,9 @@ const config: Config = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      fontFamily: {
+        minecraft: "var(--font-minecraft_font)",
+      },
     },
   },
   plugins: [],
